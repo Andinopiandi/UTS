6b8c6e64b86ca770a9f5af03633fd5428ff36a62
Andinopiandi <118586470+Andinopiandi@users.noreply.github.com>
2022-12-24 18:36:44 +0700

    tugas 11

--- Main.java
+++ Main.java
@@ -1,106 +1,94 @@
-package sesi6.bangun_ruang;
-
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        try (Scanner input = new Scanner(System.in)) {
-            while (true) {
-                System.out.println("MENGHITUNG VOLUME BANGUN RUANG");
-                System.out.println("1. KERUCUT");
-                System.out.println("2. BALOK");
-                System.out.println("3. prisma");
-                System.out.println("4. tabung");
-                System.out.println("5. bola");
-                System.out.println("6. keluar");
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
 
-                System.out.print("PILIH : ");
-                int pilih = input.nextInt();
+    // Meminta input nama, NIM, jurusan, dan tempat/tanggal lahir dari user
+    System.out.print("Nama Mahasiswa: ");
+    String nama = input.nextLine();
+    System.out.print("NIM: ");
+    String nim = input.nextLine();
+    System.out.print("Jurusan: ");
+    String jurusan = input.nextLine();
+    System.out.print("Tempat, Tanggal Lahir: ");
+    String ttl = input.nextLine();
 
-                if (pilih == 1) {
-                    System.out.println("\nMENGHITUNG VOLUME DAN LUAS KERUCUT");
-                    System.out.print("\nMasukan jari-jari : ");
-                    double jari2 = input.nextDouble();
+    // Membuat objek Mahasiswa dengan input yang diperoleh
+    Mahasiswa mhs = new Mahasiswa(nama, nim, jurusan, ttl);
 
-                    System.out.print("Masukan tinggi : ");
-                    double tinggi = input.nextDouble();
+    // Menggunakan GSON untuk mengubah objek menjadi string JSON
+    Gson gson = new Gson();
+    String jsonString = gson.toJson(mhs);
 
-                    System.out.print("Masukan panjang garis : ");
-                    double pg = input.nextDouble() ;
-
-                    Kerucut k1 = new Kerucut();
-                    k1.setJari2(jari2);
-                    k1.setTinggi(tinggi);
-                    System.out.println("\nLUASNYA ADALAH : " + k1.getLuas());
-                    System.out.println("VOLUMENYA ADALAH : "+ k1.getVolume());
-                } else if (pilih == 2) {
-                    System.out.println("\nmenghitung volume balok".toUpperCase());
-                    System.out.print("\nMasukan Panjang : ");
-                    int Panjang = input.nextInt();
-
-                    System.out.print("Masukan Lebar : ");
-                    int Lebar = input.nextInt();
-
-                    System.out.print("Masukan Tinggi : ");
-                    int Tinggi = input.nextInt();
-
-                    Balok b1 = new Balok();
-                    b1.setPanjang(Panjang);
-                    b1.setLebar(Lebar);
-                    b1.setTinggi(Tinggi);
-                    System.out.println("\nLUAS ADALAH : " + b1.getLuas());
-                    System.out.println("VOLUMENYA ADALAH : " + b1.getVolume());
-                } else if (pilih == 3) {
-                    System.out.print("luas volue prisma");
-
-                    System.out.print("masukan tinggi :");
-                    Double tinggip = input.nextDouble();
-
-                    System.out.print("masukan alas :");
-                    double alasp = input.nextDouble();
-
-                    System.out.print("masukan keliling : ");
-                    double kelilingp = input.nextDouble() ;
-
-                    Prisma p1 = new Prisma();
-                    p1.setAlasPrisma(alasp);
-                    p1.setTinggi(tinggip);
-                    p1.setkeliling(kelilingp);
-                    System.out.println("luas nya adalah : "+ p1.getLuas());
-                    System.out.println("volume nya adalah : "+ p1.getVolume());
-                    break;
-                } else if (pilih == 4) {
-                    System.out.println("\nmenghitung tabung luas volume ".toUpperCase());
-                    System.out.print("\nMasukan jari-jari : ");
-                    int jari2p = input.nextInt();
-
-                    System.out.print("Masukan Tinggi : ");
-                    int Tinggi = input.nextInt();
-
-                    Tabung t1 = new Tabung();
-                    
-                    t1.setJari2(jari2p);
-                    t1.setTinggi(Tinggi);
-                    System.out.println("\nLUAS ADALAH : " + t1.getLuas());
-                    System.out.println("VOLUMENYA ADALAH : " + t1.getVolume()); 
-                }
-                else if (pilih == 5) {
-                    System.out.println("\nmenghitung bola".toUpperCase());
-                    System.out.print("\nMasukan jari-jari : ");
-                    int jari2p = input.nextInt();
-
-                    Bola bola1 = new Bola();
-                    
-                    bola1.setJari2(jari2p);
-                    System.out.println("\nLUAS ADALAH : " + bola1.getLuas());
-                    System.out.println("VOLUMENYA ADALAH : " + bola1.getVolume()); 
-                }else if (pilih != 6){
-                    break;
-                }
-                    else {
-                    System.out.println("\nangka yg anda masukan salah\n".toUpperCase());
-                }
-            }
-        }
+    // Mencoba menulis string JSON ke file
+    try (FileWriter writer = new FileWriter("mahasiswa.json")) {
+      writer.write(jsonString);
+      System.out.println("Data berhasil disimpan ke file mahasiswa.json");
+    } catch (IOException e) {
+      System.out.println("Terjadi kesalahan saat menulis ke file: " + e.getMessage());
     }
+
+    // Mencoba membaca file JSON dan mengubah kembali menjadi objek Mahasiswa
+    try (FileReader reader = new FileReader("mahasiswa.json")) {
+      Mahasiswa mhsFromFile = gson.fromJson(reader, Mahasiswa.class);
+      System.out.println("Data Mahasiswa dari file: ");
+      System.out.println("Nama: " + mhsFromFile.getNama());
+      System.out.println("NIM: " + mhsFromFile.getNim());
+      System.out.println("Jurusan: " + mhsFromFile.getJurusan());
+      System.out.println("TTL: " + mhsFromFile.getTtl());
+    } catch (IOException e) {
+      System.out.println("Terjadi kesalahan saat membaca file: " + e.getMessage());
+    }
+  }
+}
+
+// Kelas Mahasiswa yang akan diconvert ke JSON
+class Mahasiswa {
+  private String nama;
+  private String nim;
+  private String jurusan;
+  private String ttl;
+
+  public Mahasiswa(String nama, String nim, String jurusan, String ttl) {
+    this.nama = nama;
+  this.nim = nim;
+  this.jurusan = jurusan;
+  this.ttl = ttl;
+  }
+
+  public String getNama() {
+    return nama;
+  }
+
+  public void setNama(String nama) {
+    this.nama = nama;
+  }
+
+  public String getNim() {
+    return nim;
+  }
+
+  public void setNim(String nim) {
+    this.nim = nim;
+  }
+
+  public String getJurusan() {
+    return jurusan;
+  }
+
+  public void setJurusan(String jurusan) {
+    this.jurusan = jurusan;
+  }
+
+  public String getTtl() {
+    return ttl;
+  }
+
+  public void setTtl(String ttl) {
+    this.ttl = ttl;
+  }
 }
